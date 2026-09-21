3ca0ce0012d1234a896a0b372c46fdd9deed31e8
joncharles89 <150928472+joncharles89@users.noreply.github.com>
2026-09-21 17:42:39 +0100

    Started to introduce registration and login

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js';
+
+const supabase = createClient(
+    'https://yypfjlnlwilzzvpgtbwa.supabase.co',
+    'sb_publishable_G-_r-PPQyg1CN1CwHQuqnQ_8l41VzeK'
+);
